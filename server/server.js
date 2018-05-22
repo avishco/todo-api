2044c4818976e9c0525997ba9f1ece1dff231b82
avishco <avishaibc@gmail.com>
2018-05-22 06:45:10 +0300

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require('./config/config');
-console.log(process.env.NODE_ENV);
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -118,8 +118,16 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
+  const body = _.pick(req.body, ['email', 'password']);
+
   User.findByCredentials(body.email, body.password)
+    .then( user => {
+      return user.generateAuthToken().then((token) => {
+        res.status(200).header('x-auth', token).send(user)
+      })
+    })
+    .catch( e => {
+      res.status(400).send(e)})
 });
 
 app.listen(port, () => {
